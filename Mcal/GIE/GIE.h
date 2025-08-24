d0e34b895599f4d55d2973ce002008823751d988
Hesham Ahmed <Hisham4Ahmed@gmail.com>
2025-08-25 02:18:16 +0300

    Feat  GIE Driver 4 This Commit add the GIE.h File and this Provide the to Function Like Macro (mGIE_Enable/mGIE_Disable)

---
+++ Mcal/GIE/GIE.h
@@ -0,0 +1,25 @@
+/**
+ * @file   GIE.h
+ * @author Hesham Ahmed  (Hisham4Ahmed@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-25
+ * 
+ * @copyright Copyright (c) 2025 , Gestell Company
+ * 
+ */
+
+#ifndef _GIE_H_
+#define _GIE_H_
+#include <stdint.h>
+#include "../RegistersAddress.h"
+#include "../../Common/Macros.h"
+
+
+#define I_Bit    7 
+
+#define mGIE_Enable()       SetBit(SREG_Reg,I_Bit)
+#define mGIE_Diable()       ClearBit(SREG_Reg,I_Bit)
+ 
+
+#endif/*_GIE_H_*/
