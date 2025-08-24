09bd234f6f48a206c91706e7b6c3fe5af04b35aa
Hesham Ahmed <Hisham4Ahmed@gmail.com>
2025-08-25 02:05:16 +0300

    Feat Common File 1 - in this commit i add the followint point : 1- add the Macros.h and this File Provide Some Function Like Macro (SetBit/ClearBit/ToggleBit/GetBit/ClearFlag/GetFlagstatus and provide Some Object like macro (Null/NullChar/FlagDown/FlagUp/Input/Output/Low/High/Off/On/Enable/Disbale) 2- add the RegisterAddress.h in Mcal Layer to Store the Atmega32 IO Memories Address

---
+++ Common/Macros.h
@@ -0,0 +1,50 @@
+/**
+ * @file   Macros.h
+ * @author Hesham Ahmed  (Hisham4Ahmed@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-25
+ * 
+ * @copyright Copyright (c) 2025 , Gestell Company
+ * 
+ */
+
+#ifndef _MACROS_H_
+#define _MACROS_H_
+
+
+/*Function Like Macro*/
+
+#define SetBit(Reg,BitNo)         ((Reg)|=(1<<BitNo))
+#define ClearBit(Reg,BitNo)       ((Reg)&=~(1<<BitNo))
+#define ToggleBit(Reg,BitNo)      ((Reg)^=(1<<BitNo))
+#define GetBit(Reg,BitNo)         ((Reg>>BitNo)&0x01)
+
+#define ClearFlag(Reg,FlagNo)      SetBit(Reg,FlagNo)
+#define GetFlagStatus(Reg,FlagNo)  GetBit(Reg,FlagNo);
+/*------------------------------------------------------------------------------------*/
+
+/*Object Like Macro*/
+
+#define Null              (void*)0
+#define NullChar          '\0'
+
+
+#define FlagDown            0
+#define FlagUp              1
+
+#define Input               0
+#define Output              1
+
+#define Low                 0
+#define High                1
+
+#define Off                 0
+#define On                  1
+
+#define Enable              0    
+#define Disable             1
+
+
+
+#endif /* _MACROS_H_ */
---
+++ Mcal/RegistersAddress.h
@@ -0,0 +1,24 @@
+/**
+ * @file   RegistersAddress.h
+ * @author Hesham Ahmed  (Hisham4Ahmed@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date    2025-08-25
+ * 
+ * @copyright Copyright (c) 2025 , Gestell Company
+ * 
+ */
+
+#ifndef _REGISTERADDRESS_H_
+#define _REGISTERADDRESS_H_
+#include <stdint.h>
+// Direcite Access 
+
+#define  SREG_Reg    *((volatile uint8_t *)0x5F)
+
+ // Struct 
+
+
+
+
+#endif /*_REGISTERADDRESS_H_*/
